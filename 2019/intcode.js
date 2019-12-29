07979329068659e1d0f5e83f857a338f0a17d938
Michal Szafranski <szafranski@gmail.com>
2019-12-29 16:38:30 +0100

    added clone

--- 2019/intcode.js
+++ 2019/intcode.js
@@ -1,17 +1,17 @@
-function * intcode (code) {
-    let pc = 0;
-    let rel = 0;
-    let m = code.slice()
-    const inst = () => m[pc] % 100;
-    const par = (i) => m[pc + i + 1];
-    const mpar = (i) => {
-        const mode = Math.floor(m[pc] / Math.pow(10, i+2)) % 10;
-        return mode === 1 ? par(i) : m[par(i) + (mode === 2 ? rel : 0)];
-    }
-    const rpar = (i) => {
-        const mode = Math.floor(m[pc] / Math.pow(10, i+2)) % 10;
-        return par(i) + (mode === 2 ? rel : 0);
-    }
+function icrunner(code, input = [], onData, pc = 0, rel = 0) {
+  let output = [];
+  let m = code.slice()
+  const inst = () => m[pc] % 100;
+  const par = (i) => m[pc + i + 1];
+  const mpar = (i) => {
+      const mode = Math.floor(m[pc] / Math.pow(10, i+2)) % 10;
+      return mode === 1 ? par(i) : m[par(i) + (mode === 2 ? rel : 0)];
+  }
+  const rpar = (i) => {
+      const mode = Math.floor(m[pc] / Math.pow(10, i+2)) % 10;
+      return par(i) + (mode === 2 ? rel : 0);
+  }
+  function * intcode () {
     while (inst() !== 99) {
         switch (inst()) {
             case 1: m[rpar(2)] = mpar(0) + mpar(1); pc+=4; break;
@@ -26,11 +26,10 @@ function * intcode (code) {
             default: throw("BAD INSTRUCTION " + inst())
         }
     }
-}
-function icrunner(code, input = [], onData) {
-  let output = [];
-  const exe = intcode(code);
+  }
+  const exe = intcode();
   let state = exe.next();
+  let inHanlder = false;
   function run() {
     while(!state.done) {
       let curInput = undefined;
@@ -40,8 +39,10 @@ function icrunner(code, input = [], onData) {
       } else {
         output.push(state.value);
         if (onData && output.length >= onData.length) {
+          inHanlder = true;
           onData(...output);
           output = [];
+          inHanlder = false;
         }
       }
       state = exe.next(curInput);
@@ -49,10 +50,12 @@ function icrunner(code, input = [], onData) {
   }
   run();
   return ({
-    add: (...v) => { input.push(...v); run() },
+    add: (...v) => { input.push(...v); if (!inHanlder) run() },
     get: () => output.shift(),
     getAll: () => { let o = output; output = []; return o; },
-    done: () => state.done && output.length === 0
+    done: () => state.done && output.length === 0,
+    peek: (a) => m[a],
+    clone: (input = input, onData = onData) => icrunner(m, input, onData, pc, rel),
   })
 }
 module.exports = icrunner;
