dac0fc403d2755d44c76fecee0c8343c48f94873
Michal Szafranski <szafranski@gmail.com>
2020-12-12 23:45:08 +0100

    using new graph helpers

--- 2019/6.js
+++ 2019/6.js
@@ -5,10 +5,11 @@ const content = require('fs').readFileSync('6.input.txt','utf8');
 
 const input = A.parse(content, /(\w+)\)(\w+)/g);
 function solve(input) {
-  let c = 0, n = {}
-  for (const [x,y] of input) A.connect(x,y,n);
-  A.bfs('COM', (e, d) => (c += d, n[e]));
-  const r = A.bfs('YOU', (e, d) => e === 'SAN' ? undefined : n[e]).distance - 2;
+  let c = 0;
+  const g = A.graph([], false);
+  for (const [x,y] of input) g.connect(x,y);
+  g.bfs('COM', (e, d) => c += d);
+  const r = g.bfs('YOU', (e, d) => e === 'SAN').distance - 2;
   return [c, r];
 }
 l(solve(input))
