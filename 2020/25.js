44904fa7109945d16f4cdc2762fda36f73ef2f8e
Michal Szafranski <szafranski@gmail.com>
2020-12-25 11:43:12 +0100

    2020 day 25 golf

--- 2020/25.js
+++ 2020/25.js
@@ -5,20 +5,10 @@ const content = require("fs").readFileSync("25.input.txt", "utf8");
 
 const [a, b] = A.parse(content, /(.+)/g);
 function solve() {
-  let v = 1;
+  let v;
   let c = 0;
-  while (true) {
-    if (v === a) break;
-    v *= 7;
-    v %= 20201227;
-    c++;
-  }
-  v = 1;
-  while (c) {
-    v *= b;
-    v %= 20201227;
-    c--;
-  }
+  for (v = 1; v !== a; c++) v = v * 7 % 20201227;
+  for (v = 1; c--; ) v = v * b % 20201227;
   return v;
 }
 l(solve());
